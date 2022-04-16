060582833b9a13530a4f255da5817715d25f8219
surendhar024 <86833182+surendhar024@users.noreply.github.com>
2022-04-17 00:00:39 +0530

    Update get-ssh-fingerprint.php

--- get-ssh-fingerprint.php
+++ get-ssh-fingerprint.php
@@ -1,89 +1,48 @@
-<?php
 
-/**
- * Get SSH daemon fingerprint
- * Part of the Configuration compiler for ConfigServer Firewall & LFD project
- * -----------------------------------------------------------------------------
- * By Adam "Adambean" Reece - www.reece.wales
- * https://github.com/Adambean/compile-csf
- *
- * Use this tool to fetch the fingerprint of an SSH daemon suitable for use in
- * your server list file. This will be a SHA-1 hash sum.
- *
- * Simply specify the SSH daemon address as the 1st command argument, be it an
- * IP address, host name, or FQDN. Optionally specify the port number (1-65535)
- * as the 2nd command argument. (Do not write it as host:port!)
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.
- *
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * "LICENSE" for the specific language governing permissions and limitations
- * under the License.
- */
+    <?php
+   
+$inputs = (int)readline('Enter number of total inputs: ');
 
-if (!is_array($argv) || $argc < 2) {
-    printf("[Error] No host address specified.\n");
-    exit(1);
+$array = [];
+for ($i=1; $i <= $inputs; $i++) { 
+    $array[] = (int)readline('Enter input '.$i.': ');
 }
 
-$serverHost = null;
-if (!$serverHost = trim($argv[1])) {
-    printf("[Error] Empty host address specified.\n");
-    exit(1);
-}
-
-$serverPort = 22;
-if ($argc >= 3 && !$serverPort = intval($argv[2])) {
-    $serverPort = 22;
-}
-
-if (!is_int($serverPort) || $serverPort < 1 || $serverPort > 65535) {
-    printf("[Error] Port number must be an integer from 1 to 65535 inclusive.\n");
-    exit(1);
-}
-
-
-// Show server name
-printf("%s:%d:\n\n", $serverHost, $serverPort);
-
-// << SSH connection: Open
-printf("Establishing SSH link...\n");
-
-$linkSsh        = null;
-$linkSshAuthed  = false;
-
-try {
-    if (!function_exists("ssh2_connect")) {
-        throw new \Exception("SSH2 for PHP is not installed.\n");
+for($j = 0; $j < $inputs; $j++) {
+    for($i = 0; $i < $inputs-1; $i++){
+        if($array[$i] > $array[$i+1]) {
+            $temp = $array[$i+1];
+            $array[$i+1]=$array[$i];
+            $array[$i]=$temp;
+        }       
     }
-
-    if (!$linkSsh || !is_resource($linkSsh)) {
-        $linkSsh = null;
-        $linkSsh = @ssh2_connect($serverHost, $serverPort);
-    }
-
-    // Failed completely...
-    if (!$linkSsh || !is_resource($linkSsh)) {
-        $linkSsh = null;
-        printf("[Error] SSH connection couldn't be established!\n");
-        exit(1);
-    }
-} catch (\Exception $e) {
-    printf("[Error] SSH connection failed: %s\n", $e->getMessage());
-    exit(1);
 }
 
-if (!$sshFingerprint = trim(ssh2_fingerprint($linkSsh, SSH2_FINGERPRINT_SHA1 | SSH2_FINGERPRINT_HEX))) {
-    printf("[Error] SSH server did not return a fingerprint!\n");
-    exit(1);
+
+$count = 0;
+echo "\n3 biggest odd numbers : ";
+for ($i=$inputs - 1; $i >= 0 ; $i--) { 
+    if($array[$i] % 2 != 0 && $count < 3){
+        echo $array[$i];
+        $count += 1;
+        if ($count < 3) {
+            echo ',';
+        }
+    }
 }
 
-printf("SSH fingerprint is: %s\n", $sshFingerprint);
-exit(0);
+$count = 0;
+echo "\n3 smallest odd numbers : ";
+for ($i=0; $i < $inputs; $i++) { 
+    if($array[$i] % 2 != 0 && $count < 3){
+        echo $array[$i];
+        $count += 1;
+        if ($count < 3) {
+            echo ',';
+        }
+    }
+}
+
+echo "\n";
+?>
+
